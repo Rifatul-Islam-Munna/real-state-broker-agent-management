f24758eaccf5d03a8082bd0be8191fa81e5e1d54
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-03 19:27:50 +0600

    feat(scheduling): register scheduling settings

--- backend-nestjs/src/settings/settings.module.ts
+++ backend-nestjs/src/settings/settings.module.ts
@@ -2,13 +2,21 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AgencySettings } from './entities/settings.entity';
 import { AgencyIntegrationSettings } from './entities/integration-settings.entity';
+import { AgencySchedulingSettings } from './entities/scheduling-settings.entity';
+import { SchedulingSettingsService } from './scheduling-settings.service';
 import { SettingsService } from './settings.service';
 import { SettingsController } from './settings.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AgencySettings, AgencyIntegrationSettings])],
-  providers: [SettingsService],
+  imports: [
+    TypeOrmModule.forFeature([
+      AgencySettings,
+      AgencyIntegrationSettings,
+      AgencySchedulingSettings,
+    ]),
+  ],
+  providers: [SettingsService, SchedulingSettingsService],
   controllers: [SettingsController],
-  exports: [SettingsService],
+  exports: [SettingsService, SchedulingSettingsService],
 })
 export class SettingsModule {}
