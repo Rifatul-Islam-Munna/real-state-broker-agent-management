f39730633cb2f149c0b6c221f3283eff48b49053
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-03 18:41:35 +0600

    fix(ui): update property search section export

--- frontend/components/stitch/pages/advanced-property-search/sections/index.ts
+++ frontend/components/stitch/pages/advanced-property-search/sections/index.ts
@@ -1,2 +1,2 @@
 export { TopNavigationSection } from "./top-navigation"
-export { MainContentAreaSplitViewSection } from "./main-content-area-split-view"
+export { MainContentAreaSplitViewSection } from "./main-content-area-v2"
