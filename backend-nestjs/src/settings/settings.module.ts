bc914c7822e153da5b6f78f7cc4aeeb8f40a8d72
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-03 19:45:23 +0600

    fix(settings): use JSON scheduling service

--- backend-nestjs/src/settings/settings.module.ts
+++ backend-nestjs/src/settings/settings.module.ts
@@ -2,19 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AgencySettings } from './entities/settings.entity';
 import { AgencyIntegrationSettings } from './entities/integration-settings.entity';
-import { AgencySchedulingSettings } from './entities/scheduling-settings.entity';
-import { SchedulingSettingsService } from './scheduling-settings.service';
+import { SchedulingSettingsService } from './scheduling-settings-json.service';
 import { SettingsService } from './settings.service';
 import { SettingsController } from './settings.controller';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([
-      AgencySettings,
-      AgencyIntegrationSettings,
-      AgencySchedulingSettings,
-    ]),
-  ],
+  imports: [TypeOrmModule.forFeature([AgencySettings, AgencyIntegrationSettings])],
   providers: [SettingsService, SchedulingSettingsService],
   controllers: [SettingsController],
   exports: [SettingsService, SchedulingSettingsService],
