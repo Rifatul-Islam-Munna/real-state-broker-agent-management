b88253bd43b29a82099b8dea1ca50620aac828fb
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-03 21:45:13 +0600

    feat(integrations): register health controller

--- backend-nestjs/src/settings/settings.module.ts
+++ backend-nestjs/src/settings/settings.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AgencySettings } from './entities/settings.entity';
 import { AgencyIntegrationSettings } from './entities/integration-settings.entity';
+import { IntegrationHealthController } from './integration-health.controller';
 import { IntegrationHealthService } from './integration-health.service';
 import { IntegrationWorkspaceService } from './integration-workspace.service';
 import { SchedulingSettingsService } from './scheduling-settings-json.service';
@@ -11,7 +12,7 @@ import { SettingsController } from './settings.controller';
 @Module({
   imports: [TypeOrmModule.forFeature([AgencySettings, AgencyIntegrationSettings])],
   providers: [SettingsService, SchedulingSettingsService, IntegrationWorkspaceService, IntegrationHealthService],
-  controllers: [SettingsController],
+  controllers: [SettingsController, IntegrationHealthController],
   exports: [SettingsService, SchedulingSettingsService, IntegrationHealthService],
 })
 export class SettingsModule {}
