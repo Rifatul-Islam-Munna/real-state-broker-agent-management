7a64a4d474f69db7b7cab7825bd191f02b40dcf3
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-03 21:38:06 +0600

    feat(leads): connect property chat automation

--- backend-nestjs/src/property-chat/property-chat.module.ts
+++ backend-nestjs/src/property-chat/property-chat.module.ts
@@ -5,11 +5,12 @@ import { PropertyChatService } from './property-chat.service';
 import { PropertyChatController } from './property-chat.controller';
 import { Lead } from '../leads/entities/lead.entity';
 import { LeadHistoryEntry } from '../leads/entities/lead-history.entity';
+import { LeadsModule } from '../leads/leads.module';
 import { Property } from '../properties/entities/property.entity';
 import { SettingsModule } from '../settings/settings.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PropertyChatConversation, PropertyChatMessage, Lead, LeadHistoryEntry, Property]), SettingsModule],
+  imports: [TypeOrmModule.forFeature([PropertyChatConversation, PropertyChatMessage, Lead, LeadHistoryEntry, Property]), SettingsModule, LeadsModule],
   providers: [PropertyChatService],
   controllers: [PropertyChatController],
 })
