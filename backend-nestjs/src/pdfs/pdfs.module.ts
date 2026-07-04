ab54541e31e7a80e0c9bde92df72c103351aaaad
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-04 16:31:32 +0600

    feat(pdfs): add backend PDFs module

---
+++ backend-nestjs/src/pdfs/pdfs.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FileUploadModule } from '../file-upload/file-upload.module';
+import { Lead } from '../leads/entities/lead.entity';
+import { Property } from '../properties/entities/property.entity';
+import { AgencySettings } from '../settings/entities/settings.entity';
+import { User } from '../users/entities/user.entity';
+import { PdfGeneration } from './entities/pdf-generation.entity';
+import { PdfTemplate } from './entities/pdf-template.entity';
+import { PdfsController } from './pdfs.controller';
+import { PdfsService } from './pdfs.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      PdfTemplate,
+      PdfGeneration,
+      Property,
+      Lead,
+      User,
+      AgencySettings,
+    ]),
+    FileUploadModule,
+  ],
+  controllers: [PdfsController],
+  providers: [PdfsService],
+  exports: [PdfsService],
+})
+export class PdfsModule {}
