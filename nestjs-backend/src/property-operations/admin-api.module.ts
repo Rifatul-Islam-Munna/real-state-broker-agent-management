4c0bc674427a4c9d45354c5d6b13459a0b9b9b57
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-05 22:59:20 +0600

    refactor(nestjs): register PostgreSQL admin API modules

--- nestjs-backend/src/property-operations/admin-api.module.ts
+++ nestjs-backend/src/property-operations/admin-api.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { DatabaseCoreModule } from '../core/database-core.module';
 import { ExternalCoreModule } from '../core/external-core.module';
+import { ExternalAdminController } from './controllers/external-admin.controller';
 import { JobsController } from './controllers/jobs.controller';
 import { ManagementController } from './controllers/management.controller';
-import { PublicAdminController } from './controllers/public-admin.controller';
-import { UploadController } from './controllers/upload.controller';
+import { UploadPgController } from './controllers/upload-pg.controller';
 import { WorkspaceRecordController } from './controllers/workspace-record.controller';
+import { RecordApiModule } from './record-api.module';
 
 @Module({
-  imports: [DatabaseCoreModule, ExternalCoreModule],
-  controllers: [WorkspaceRecordController, ManagementController, PublicAdminController, UploadController, JobsController],
+  imports: [DatabaseCoreModule, ExternalCoreModule, RecordApiModule],
+  controllers: [WorkspaceRecordController, ManagementController, ExternalAdminController, UploadPgController, JobsController],
 })
 export class AdminApiModule {}
