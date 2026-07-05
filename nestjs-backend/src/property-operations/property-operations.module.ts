caea513b83e0e9438815cdd77e243107fbe5c231
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-06 02:05:52 +0600

    remove obsolete module

--- nestjs-backend/src/property-operations/property-operations.module.ts
+++
@@ -1,6 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DatabaseCoreModule } from '../core/database-core.module';
-import { ExternalCoreModule } from '../core/external-core.module';
-
-@Module({ imports: [DatabaseCoreModule, ExternalCoreModule] })
-export class PropertyOperationsModule {}
