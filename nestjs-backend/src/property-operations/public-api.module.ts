20adc95382d309bebcd89e2b8dc6aa602853753b
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-05 22:58:02 +0600

    refactor(nestjs): register PostgreSQL public controllers

--- nestjs-backend/src/property-operations/public-api.module.ts
+++ nestjs-backend/src/property-operations/public-api.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ExternalCoreModule } from '../core/external-core.module';
-import { PublicController } from './controllers/public.controller';
+import { ExternalPublicController } from './controllers/external-public.controller';
 import { PublicStatusController } from './controllers/public-status.controller';
 
-@Module({ imports: [ExternalCoreModule], controllers: [PublicController, PublicStatusController] })
+@Module({ imports: [ExternalCoreModule], controllers: [ExternalPublicController, PublicStatusController] })
 export class PublicApiModule {}
