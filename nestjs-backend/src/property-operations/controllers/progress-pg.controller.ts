2f05addd4dbdf48690a4a34788384e7fa0904e1b
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-05 22:51:28 +0600

    wire PostgreSQL progress dependency

--- nestjs-backend/src/property-operations/controllers/progress-pg.controller.ts
+++ nestjs-backend/src/property-operations/controllers/progress-pg.controller.ts
@@ -1,1 +1,7 @@
-export class ProgressPgController {}
+import { Body, Controller, Get, Param, Patch } from '@nestjs/common';
+import { ExternalStatusService } from '../../core/external-status.service';
+
+@Controller('property-operations/public/:token/status')
+export class ProgressPgController {
+  constructor(private readonly progress: ExternalStatusService) {}
+}
