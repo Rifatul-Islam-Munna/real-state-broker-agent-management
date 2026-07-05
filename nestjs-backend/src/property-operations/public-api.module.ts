999f7e41a3e7e479c06335de08c6b3b1e8ceea6c
Rifatul Islam <88204207+Rifatul-Islam-Munna@users.noreply.github.com>
2026-07-06 02:04:42 +0600

    delete unused source file

--- nestjs-backend/src/property-operations/public-api.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ExternalCoreModule } from '../core/external-core.module';
-import { ExternalPublicController } from './controllers/external-public.controller';
-import { PublicStatusController } from './controllers/public-status.controller';
-
-@Module({ imports: [ExternalCoreModule], controllers: [ExternalPublicController, PublicStatusController] })
-export class PublicApiModule {}
